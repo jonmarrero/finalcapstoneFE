9a07042f3efbfbcdcbc4669c96c04662463f044b
Jonathan Marrero <Jonathanmarrero24@gmail.com>
2023-03-25 16:32:10 -0400

    update changes

--- src/actions.js
+++ src/actions.js
@@ -34,7 +34,7 @@ export async function UpdateAction({ request, params }) {
     };
   
     // request to update route in backend
-    await fetch(url + "/zooss/" + params.id + "/", {
+    await fetch(url + "/zoos/" + params.id + "/", {
       method: "put",
       headers: {
         "Content-Type": "application/json",
