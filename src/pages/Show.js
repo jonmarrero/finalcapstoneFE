c6b3b54ab82632f3a604935ff5ffa8faeacc9f76
Jonathan Marrero <Jonathanmarrero24@gmail.com>
2023-03-29 20:31:57 -0400

    more SASS added

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,4 +1,5 @@
 import { Link, useLoaderData, Form } from "react-router-dom";
+// import { SearchOutlined, ShoppingBasket } from '@material-ui/icons'
 
 const Show = (props) => {
   const post = useLoaderData();
@@ -14,6 +15,7 @@ const Show = (props) => {
   return (
     <div style={div}>
       <h1>{post.subject}</h1>
+
       <h2>{post.details}</h2>
       <img src="https://media.istockphoto.com/id/451027665/vector/illustration-of-the-strong-garilly.jpg?b=1&s=612x612&w=0&k=20&c=mBULDJT4TsdwaVtJbcB4-pnIYl4uCI0OMQgixPii06k="></img>
       <div>
