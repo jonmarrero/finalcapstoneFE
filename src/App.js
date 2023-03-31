97f513cb2e085fd31d960d98ac872721e2f6e09b
Jonathan Marrero <Jonathanmarrero24@gmail.com>
2023-03-31 18:46:37 -0400

    nav bar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import { Outlet } from 'react-router-dom';
 import './App.scss';
 import Slider from './components/Slider';
 import { Data } from './components/Data';
+// import { useState } from 'react';
 
 function App() {
+  // const [query, setQuery] = useState(""
+  // )
   return (
     <div className="App">
       <h1 className='zoo'>THE ZOO</h1>
-      <h5>" Unleash Your Inner BEAST "</h5>
+      <h4>" Unleash Your Inner BEAST "</h4>
+      <div className='search'>Search</div>
+      <div className='login'>Log In / Sign-Up</div>
+      <div className='programs'>Programs</div>
+      <div className='meals'>Meal Plans</div>
+      {/* <input type="text" onChange={e => setQuery(e.target.value)} /> */}
       <Slider slides={Data} />
       <Outlet/>
     </div>
